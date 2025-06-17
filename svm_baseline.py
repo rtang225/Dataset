a7138203e8eb8cd989b2042fe4740cc2cfb778a3
connor_mcjesus <rtang225@uwo.ca>
2025-06-17 15:43:02 -0400

    testing svm again cause i deleted results the first time..... im an idiot

--- svm_baseline.py
+++ svm_baseline.py
@@ -9,13 +9,17 @@ from imblearn.over_sampling import SMOTE
 
 # Load the dataset
 file_path = 'datasetreduced.csv'
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path, usecols=['temperature_2m_mean','wind_speed_10m_max', 'relative_humidity_2m_mean', 'wind_speed_10m_mean', 'vapour_pressure_deficit_max', 'area_class', 'apparent_temperature_mean', 'rain_sum', 'soil_moisture_0_to_7cm_mean', 'soil_moisture_7_to_28cm_mean', 'dew_point_2m_mean'])#, 'wind_gusts_10m_max', 'wind_gusts_10m_mean', 'soil_moisture_0_to_100cm_mean', 'wet_bulb_temperature_2m_mean'])#, 'vNDVI', 'VARI'])
 print(df.head())
 
 # Prepare features and target
 y = df['area_class']
+X = df.drop(['area_class'], axis=1, errors='ignore')
+"""
+# Prepare features and target
+y = df['area_class']
 X = df.drop(['area_class', 'date', 'latitude', 'longitude', 'vNDVI', 'VARI'], axis=1, errors='ignore')
-
+"""
 # Encode target if not numeric
 if y.dtype == 'O' or y.dtype.name == 'category':
     le = LabelEncoder()
